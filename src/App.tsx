8d2e95851adbba9e656dd8b8fd535235a0a906cf
Tejasvita <ttejasvita@gmail.com>
2026-01-30 18:58:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-blue-50">
+      <h1 className="font-bold text-3xl bg-gradient-to-r from-purple-300 via-purple-500 to-purple-300 text-white p-5 text-center">
+        SHOPPING APP
+      </h1>
+      <div className="flex">
+        {/* <Filters
+          cols={cols}
+          setCols={setCols}
+          filters={filters}
+          setFilters={setFilters}
+        />
+        <Cards cols={cols} filters={filters} /> */}
+      </div>
+    </div>
+  )
+}
+
+export default App
